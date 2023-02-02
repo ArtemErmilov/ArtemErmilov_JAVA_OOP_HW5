170dd7b792f5f2c787fa6e1c62bf459a9fa289de
Artem Ermilov <ermilo-artjom@rambler.ru>
2023-02-02 19:17:45 +0300

    JAVA OOP ДЗ 5

---
+++ Model/Method/CalcModel.java
@@ -0,0 +1,10 @@
+package Homework.JAVA_HW11_OOP.Model.Method;
+
+public abstract class CalcModel implements ICalcModel{
+
+  
+    public Integer[] xRN;
+
+    public Integer[] yRN;
+    
+}
---
+++ Model/Method/ICalcModel.java
@@ -0,0 +1,18 @@
+package Homework.JAVA_HW11_OOP.Model.Method;
+
+public interface ICalcModel {
+ 
+
+ void setX(Integer[]  value);
+
+ void setY(Integer[]  value);
+ 
+ Integer[] getSum();
+
+ Integer[] getSub();
+
+ Integer[] getMult();
+
+ Integer[] getDiv();
+    
+}
